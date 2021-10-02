f8ee7fc95b3a0591335faa24468768b78b8c4ee5
CyanMarine <58150571+rawancer@users.noreply.github.com>
2021-10-02 15:05:50 -0300

    Delete writeServiceFile.js

--- writeServiceFile.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", {
-    value: true
-});
-var dotenv_1 = require("dotenv");
-var fs_1 = require("fs");
-(0, dotenv_1.config)();
-if (process.env.GOOGLE_APPLICATION_CREDENTIALS && process.env.SERVICEACCOUNT)
-    (0, fs_1.writeFileSync)(process.env.GOOGLE_APPLICATION_CREDENTIALS, process.env.SERVICEACCOUNT.replace(/\$\(INSETNEWLINEHERE\)/g, '\n'));
