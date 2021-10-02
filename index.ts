a3dcfb747f77bb6814d07ca7c308cf82fe702335
CyanMarine <58150571+rawancer@users.noreply.github.com>
2021-10-02 15:17:02 -0300

    commit

--- index.ts
+++ index.ts
@@ -1,3 +1,7 @@
 import "./krystal/index"
 import "./sadie/index"
 import "./d20/index"
+import { readFileSync } from "fs"
+
+if (process.env.GOOGLE_APPLICATION_CREDENTIALS) console.log(readFileSync(process.env.GOOGLE_APPLICATION_CREDENTIALS, 'utf-8'));
+else console.log('A');
