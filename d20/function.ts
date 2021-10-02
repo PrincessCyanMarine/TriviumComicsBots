d87e41f0e378cdba942fd33a2d2cd8b5de779444
CyanMarine <58150571+rawancer@users.noreply.github.com>
2021-10-02 15:22:27 -0300

    commit

--- d20/function.ts
+++ d20/function.ts
@@ -1,11 +1,11 @@
 import { Message } from "discord.js";
-import { d20, database } from "../clients";
+// import { d20, database } from "../clients";
 import { say } from "../common/functions";
 import { not_count_in_channel_ids } from "../common/variables";
 
 export async function countMessages(msg: Message) {
-    if (not_count_in_channel_ids.includes(msg.channel.id)) return;
-    let messages = await (await database.child(`lvl/${msg.author.id}`).once('value')).val();
-    messages++;
-    database.child(`lvl/${msg.author.id}`).set(messages);
+    // if (not_count_in_channel_ids.includes(msg.channel.id)) return;
+    // let messages = await (await database.child(`lvl/${msg.author.id}`).once('value')).val();
+    // messages++;
+    // database.child(`lvl/${msg.author.id}`).set(messages);
 }
