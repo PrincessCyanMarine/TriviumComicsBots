780d1b99921e503ffa0c3028faf1bd42c2ee84e9
CyanMarine <rawanrios@gmail.com>
2021-10-19 14:32:38 -0300

    Nitro.com

--- src/d20/commandHandler.ts
+++ src/d20/commandHandler.ts
@@ -3,5 +3,5 @@ import { Message } from "discord.js";
 export function testCommands(msg: Message) {
     let args = msg.content.toLowerCase();
 
-    if (args.match(/nitro/gi)) return msg.channel.send('<@&609593848448155668> please confirm this isn\'t a free nitro scam');
+    if (args.match(/nitro/gi) && args.match(/.+?\..+?/gi)) return msg.channel.send('<@&609593848448155668> please confirm this isn\'t a free nitro scam');
 }
