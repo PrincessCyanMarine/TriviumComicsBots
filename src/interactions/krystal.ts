c41dfa6a692de839f348716394fd7b850f70be9f
CyanMarine <rawanrios@gmail.com>
2021-10-20 10:13:51 -0300

    alert

--- src/interactions/krystal.ts
+++ src/interactions/krystal.ts
@@ -10,8 +10,9 @@ krystal.on('interactionCreate', async (interaction) => {
 
     switch (interaction.customId) {
         case "gamemastersfanrole":
-            if (!interaction.guildId || !interaction.guild || !interaction.member || !(interaction.member instanceof GuildMember) || triviumGuildId != interaction.guildId) return;
-            let role_id = "774127564675481600";
+        case "queensbladetogglealerts":
+            if (!interaction.guildId || !interaction.guild || !interaction.member || !(interaction.member instanceof GuildMember) || (triviumGuildId != interaction.guildId && interaction.guildId != "900362801992835073")) return;
+            let role_id = interaction.guildId == triviumGuildId ? "774127564675481600" : "620088019868844042";
             let role = interaction.guild.roles.cache.get(role_id);
             if (!role) return;
             let roles = interaction.member.roles;
