30dfb7c0a476836f7d73e2128a0c44c1fcf2ec26
CyanMarine <rawanrios@gmail.com>
2021-10-20 10:18:30 -0300

    mod mail

--- src/interactions/krystal.ts
+++ src/interactions/krystal.ts
@@ -7,12 +7,13 @@ krystal.on('interactionCreate', async (interaction) => {
     if (!interaction.isButton()) return;
     if (testing && interaction.channelId != '892800588469911663') return;
     else if (!testing && interaction.channelId == '892800588469911663') return;
-
+    console.log(interaction.customId);
     switch (interaction.customId) {
         case "gamemastersfanrole":
         case "queensbladetogglealerts":
-            if (!interaction.guildId || !interaction.guild || !interaction.member || !(interaction.member instanceof GuildMember) || (triviumGuildId != interaction.guildId && interaction.guildId != "900362801992835073")) return;
-            let role_id = interaction.guildId == triviumGuildId ? "774127564675481600" : "620088019868844042";
+            let correctGuild = interaction.customId == "gamemastersfanrole" ? triviumGuildId : "620088019868844042";
+            if (!interaction.guildId || !interaction.guild || !interaction.member || !(interaction.member instanceof GuildMember) || correctGuild != interaction.guildId) return;
+            let role_id = interaction.customId == "gamemastersfanrole" ? "774127564675481600" : "900363259188772865";
             let role = interaction.guild.roles.cache.get(role_id);
             if (!role) return;
             let roles = interaction.member.roles;
