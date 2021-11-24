d2d249df64d672c71b3d77529e50ee9f77d94368
CyanMarine <rawanrios@gmail.com>
2021-11-24 19:05:49 -0300

    changed client init and added cerby and sieg

--- src/clients.ts
+++ src/clients.ts
@@ -3,51 +3,32 @@ import { config } from "dotenv";
 import { changeActivities } from "./common/functions";
 config();
 
-const INTENTS = [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_INTEGRATIONS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_PRESENCES];
+const intents = [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_INTEGRATIONS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_PRESENCES];
 
-export const krystal = new Client({
-    intents: INTENTS
-});
-krystal.login(process.env.BOT_KRYSTAL_TOKEN);
-krystal.on('ready', () => {
-    console.log(`${krystal.user ? krystal.user.tag : "Sadie"} is ready!!!`);
-    changeActivities();
-});
+export var krystal = new Client({ intents }),
+    sadie = new Client({ intents }),
+    ray = new Client({ intents }),
+    eli = new Client({ intents }),
+    d20 = new Client({ intents }),
+    cerby = new Client({ intents }),
+    sieg = new Client({ intents });
 
-export const sadie = new Client({
-    intents: INTENTS
-});
-sadie.login(process.env.BOT_SADIE_TOKEN);
-sadie.on('ready', () => {
-    console.log(`${sadie.user ? sadie.user.tag : "Sadie"} is ready!!!`);
-    changeActivities();
-});
+krystal.login(process.env.BOT_KRYSTAL_TOKEN)
+sadie.login(process.env.BOT_SADIE_TOKEN)
+ray.login(process.env.BOT_RAY_TOKEN)
+eli.login(process.env.BOT_ELI_TOKEN)
+d20.login(process.env.BOT_D20_TOKEN)
+cerby.login(process.env.BOT_CERBY_TOKEN)
+sieg.login(process.env.BOT_SIEG_TOKEN)
 
-export const d20 = new Client({
-    intents: INTENTS
-});
-d20.login(process.env.BOT_D20_TOKEN);
-d20.on('ready', () => { console.log(`${d20.user ? d20.user.tag : "D20"} is ready!!!`) });
-
-
-export const ray = new Client({
-    intents: INTENTS
-});
-ray.login(process.env.BOT_RAY_TOKEN);
-ray.on('ready', () => {
-    console.log(`${ray.user ? ray.user.tag : "Ray"} is ready!!!`);
-    changeActivities();
-});
-
-
-export const eli = new Client({
-    intents: INTENTS
-});
-eli.login(process.env.BOT_ELI_TOKEN);
-eli.on('ready', () => {
-    console.log(`${eli.user ? eli.user.tag : "Eli"} is ready!!!`);
-    changeActivities();
-});
+var client_list = [krystal, sadie, ray, eli, cerby, sieg];
+client_list.forEach(client => {
+    client.on('ready', () => {
+        if (!client.user) throw ("Something went wrong with client init");
+        console.log(`${client.user.tag} is ready!!!`);
+        changeActivities();
+    });
+})
 
 export const mod_alert_webhook = (testing: boolean) => testing ?
     new WebhookClient({
@@ -63,6 +44,8 @@ export const clients: { [bot: string]: Client } = {
     'krystal': krystal,
     'eli': eli,
     'ray': ray,
+    'cerberus': cerby,
+    'siegfried': sieg,
 }
 
 // IDs are already public
@@ -72,6 +55,8 @@ export const id2bot: { [bot: string]: string } = {
     "620634675454541844": 'krystal',
     "666872683530813441": 'eli',
     "666795899879424020": 'ray',
+    "711241945149734914": 'cerberus',
+    "723938416139567154": 'siegfried',
 }
 
 export type CustomActivity = [string, Exclude<ActivityType, "CUSTOM">, string, (string | Buffer)?, string?, string?];
