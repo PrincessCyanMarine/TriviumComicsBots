8513d2befa9ce1ccd1e95dcf38c781802e403b25
CyanMarine <rawanrios@gmail.com>
2021-12-21 00:57:17 -0300

    M̵̝̬̓͊͗̎̽̈͝M̵̻̙̜͖̺͔͖̳͘M̴̧̨͖̖̣̩̣̙̳̗̖̰̫͙̺̃̑̆̂́̾̚

--- src/express.ts
+++ src/express.ts
@@ -283,7 +283,7 @@ express_app.get("/birddex/:id?/:guild_id?", async (req, res) => {
       bird_list = [
         {
           url: "https://cyanmarine.net",
-          bird: "Princess CyanMarine",
+          bird: "P̷͇͙̮̦̹͌̊͌̓̀̒̈͆̽̔͠ṙ̶̠̲̳̘̝̹͇͇̩̘̈͘͜ͅi̸̧̛̭̱͉̗̫͉͓̝̋́̄̎͋́̅͆̇͛͛̏̕n̴̖̻̹̗̮̭̍́̊̃͊c̷͙̳̮̺̙͓̊̐̆͛̒̋̈́͠e̵̢̫̱͓̭͈̘̪̺͇̰͕̤͙̞̓̓̈̄͆͌̅͝s̵͈̦̳̖̔͒͒͛͑̄͐̍͛͝s̵̡̛̝͉̲͖͚̜͙͎̮͙͛ͅ ̵̡̩̬̰̥̠̞̼̲͎̟͉͕͈̏̆͆͊͗͑͊̑͛̃̿̀͝ͅM̷̡̝̫̈́̑̈́̅̒̚͠ͅa̷̢̛͉̾͛̍̄̏̑͝r̸̜̤̀͐̿i̴̗̜̱̜̜͙͍͇̦̪̬͎̳̮͐͂̿̎̓̾̄͒̃͐͜n̵̨̦̮̜̹̝̘͖̥̈ȩ̸̳̬̩̀",
           img: "https://cdn.discordapp.com/avatars/305883924310261760/c9c990b93fe013c6599d26d96258916f.png?size=1024",
           snippet: "Princess of the kingdom of Cyan. Got bored from the life of royalty and decided to become a pirate.",
         },
