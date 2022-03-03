36a529739464f028e61a329715daa7ad2f59d60d
CyanMarine <58150571+PrincessCyanMarine@users.noreply.github.com>
2022-03-03 17:09:52 -0300

    Added testing  check for Calculator buttons

--- src/eli/index.ts
+++ src/eli/index.ts
@@ -1,6 +1,6 @@
 import { testing } from "..";
 import { eli } from "../clients";
-import { ignore_message, msg2embed } from "../common/functions";
+import { ignore_message } from "../common/functions";
 import { ignore_channels, testChannelId } from "../common/variables";
 import { Calculator } from "../games/calculator";
 import { testCommands } from "./commandHandler";
@@ -12,12 +12,15 @@ eli.on("messageCreate", (msg) => {
 
 eli.on("interactionCreate", (interaction) => {
     if (interaction.isButton()) {
+        if (testing && interaction.channelId != testChannelId) return;
+        if (!testing && interaction.channelId == testChannelId) return;
+
         let match = interaction.customId.match(/calculator_(?<id>[0-9]+?)_button_(?<button>.+)/);
         if (match?.groups) {
-            if (match.groups.id != interaction.member?.user.id) {
+            /*if (match.groups.id != interaction.member?.user.id) {
                 interaction.reply({ ephemeral: true, content: 'To invoke a calculator, use the command "!calculator"' });
                 return;
-            }
+            }*/
 
             Calculator.processInteraction(interaction, match.groups.button);
         }
