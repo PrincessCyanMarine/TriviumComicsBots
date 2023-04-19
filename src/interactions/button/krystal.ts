cbeafe966b28987cb9aa81ca99d064feb80e4623
CyanMarine <58150571+PrincessCyanMarine@users.noreply.github.com>
2023-04-18 23:55:52 -0300

    a

--- src/interactions/button/krystal.ts
+++ src/interactions/button/krystal.ts
@@ -4,7 +4,6 @@ import { krystal } from "../../clients";
 import { colors, triviumGuildId } from "../../common/variables";
 
 krystal.on("interactionCreate", async (interaction) => {
-    console.log(interaction);
     if (!interaction.isButton()) return;
     if (testing && interaction.channelId != "892800588469911663") return;
     else if (!testing && interaction.channelId == "892800588469911663") return;
@@ -44,62 +43,6 @@ krystal.on("interactionCreate", async (interaction) => {
         } catch (e) {
             interaction.reply({ content: "Something went wrong", ephemeral: true });
             console.error(e);
-        }
-    }
-
-    switch (interaction.customId) {
-        case "gamemastersfanrole":
-        case "queensbladetogglealerts": {
-            let correctGuild = interaction.customId == "gamemastersfanrole" ? triviumGuildId : "620088019868844042";
-            if (
-                !interaction.guildId ||
-                !interaction.guild ||
-                !interaction.member ||
-                !(interaction.member instanceof GuildMember) ||
-                correctGuild != interaction.guildId
-            )
-                return;
-            let role_id = interaction.customId == "gamemastersfanrole" ? "774127564675481600" : "900363259188772865";
-            let role = await interaction.guild.roles.fetch(role_id);
-            if (!role) return;
-            let roles = interaction.member.roles;
-            if (!roles.cache.has(role_id))
-                roles.add(role).then(() => {
-                    interaction.reply({ content: `<@&${role_id}> role added!`, ephemeral: true });
-                });
-            else
-                roles.remove(role).then(() => {
-                    interaction.reply({ content: `<@&${role_id}> role removed!`, ephemeral: true });
-                });
-            break;
-        }
-        case "geminitwilightfanrole": {
-            let correctGuild = triviumGuildId;
-            if (
-                !interaction.guildId ||
-                !interaction.guild ||
-                !interaction.member ||
-                !(interaction.member instanceof GuildMember) ||
-                correctGuild != interaction.guildId
-            )
-                return;
-            let role_id = "819361862474661890";
-            let role = await interaction.guild.roles.fetch(role_id);
-            if (!role) return;
-            let roles = interaction.member.roles;
-            try {
-                if (!roles.cache.has(role_id)) {
-                    await roles.add(role);
-                    interaction.reply({ content: `<@&${role_id}> role added!`, ephemeral: true });
-                } else {
-                    await roles.remove(role);
-                    interaction.reply({ content: `<@&${role_id}> role removed!`, ephemeral: true });
-                }
-            } catch (err) {
-                console.error(err);
-                interaction.reply({ content: `Interaction failed...`, ephemeral: true });
-            }
-            break;
         }
     }
 });
--- src/interactions/button/ray.ts
+++ src/interactions/button/ray.ts
@@ -1,8 +1,8 @@
-import { Message } from "discord.js";
+import { GuildMember, Message } from "discord.js";
 import { testing } from "../..";
 import { ray } from "../../clients";
 import { Help } from "../../common/help";
-import { testChannelId } from "../../common/variables";
+import { testChannelId, triviumGuildId } from "../../common/variables";
 import { get_rank_message } from "../../d20/functions";
 
 ray.on("interactionCreate", async (interaction) => {
@@ -18,5 +18,61 @@ ray.on("interactionCreate", async (interaction) => {
         // if (rank_match.groups["userId"] != interaction.user.id) { interaction.reply({ content: "You can't do that", ephemeral: true }); return }
         // console.log(rank_match.groups);
         interaction.update(await get_rank_message(interaction.guild, interaction.user.id, undefined, parseInt(rank_match.groups["page"])));
+    }
+
+    switch (interaction.customId) {
+        case "gamemastersfanrole":
+        case "queensbladetogglealerts": {
+            let correctGuild = interaction.customId == "gamemastersfanrole" ? triviumGuildId : "620088019868844042";
+            if (
+                !interaction.guildId ||
+                !interaction.guild ||
+                !interaction.member ||
+                !(interaction.member instanceof GuildMember) ||
+                correctGuild != interaction.guildId
+            )
+                return;
+            let role_id = interaction.customId == "gamemastersfanrole" ? "774127564675481600" : "900363259188772865";
+            let role = await interaction.guild.roles.fetch(role_id);
+            if (!role) return;
+            let roles = interaction.member.roles;
+            if (!roles.cache.has(role_id))
+                roles.add(role).then(() => {
+                    interaction.reply({ content: `<@&${role_id}> role added!`, ephemeral: true });
+                });
+            else
+                roles.remove(role).then(() => {
+                    interaction.reply({ content: `<@&${role_id}> role removed!`, ephemeral: true });
+                });
+            break;
+        }
+        case "geminitwilightfanrole": {
+            let correctGuild = triviumGuildId;
+            if (
+                !interaction.guildId ||
+                !interaction.guild ||
+                !interaction.member ||
+                !(interaction.member instanceof GuildMember) ||
+                correctGuild != interaction.guildId
+            )
+                return;
+            let role_id = "819361862474661890";
+            let role = await interaction.guild.roles.fetch(role_id);
+            if (!role) return;
+            let roles = interaction.member.roles;
+            try {
+                if (!roles.cache.has(role_id)) {
+                    await roles.add(role);
+                    interaction.reply({ content: `<@&${role_id}> role added!`, ephemeral: true });
+                } else {
+                    await roles.remove(role);
+                    interaction.reply({ content: `<@&${role_id}> role removed!`, ephemeral: true });
+                }
+            } catch (err) {
+                console.error(err);
+                interaction.reply({ content: `Interaction failed...`, ephemeral: true });
+            }
+            break;
+        }
     }
 });
