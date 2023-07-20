9b41385022b16774dbfb1a6cfc311cae76d4a06c
CyanMarine <58150571+PrincessCyanMarine@users.noreply.github.com>
2023-07-20 12:50:28 -0300

    a

--- src/common/variables.ts
+++ src/common/variables.ts
@@ -5,8 +5,8 @@ import { testing } from "..";
 
 export const testGuildId: string = "620635349173010465";
 export const triviumGuildId: string = "562429293364248587";
-export const testChannelId: string = "892800588469911663";
-// export const testChannelId: string = "999917475342843924";
+// export const testChannelId: string = "892800588469911663";
+export const testChannelId: string = "999917475342843924";
 
 export const sadieId: string = "622898538514350085";
 export const krystalId: string = "620634675454541844";
