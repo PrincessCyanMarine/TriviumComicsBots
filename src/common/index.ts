75c67ffd3e066ae5efcb134247254227d3772c58
CyanMarine <58150571+PrincessCyanMarine@users.noreply.github.com>
2023-07-22 22:56:21 -0300

    !birddex button removed

--- src/common/index.ts
+++ src/common/index.ts
@@ -267,40 +267,7 @@ d20.on("messageCreate", async (msg) => {
                         : random_from_array(acts[options[1]]);
                 changeActivity(...act);
                 break;
-            case "bird":
-            case "birds":
-            case "birdpedia":
-            case "birddex":
-            case "birdex":
-            case "bp":
-            case "birdwiki":
-                {
-                    // say(d20, msg.channel, "Currently unavailable");
-                    // return;
-                    let selected_bird = parseInt(options[1]) || -1;
-                    let bird_list = get_birds();
-                    if (selected_bird > -1 && selected_bird < bird_list.length) {
-                        let bird = bird_list[selected_bird];
-                        say(krystal, msg.channel, bird.bird + "\n" + bird.url);
-                        return;
-                    }
-                    let target = msg.mentions.members?.first() || msg.member;
-                    let birds = Object.entries((await database.child("birdpedia/" + msg.guild!.id + "/" + target.id).once("value")).val() || {});
-                    let percentage = Math.floor((birds.length / bird_list.length) * 100);
-                    say(krystal, msg.channel, {
-                        content: `${target.displayName} found ${birds.length} out of the birddex's ${bird_list.length} birds (${percentage})% full\n`,
-                        components: [
-                            new MessageActionRow().addComponents(
-                                new MessageButton()
-                                    .setStyle("LINK")
-                                    .setLabel(`See ${target.displayName}'s birddex`)
-                                    .setURL(`https://www.cyanmarine.net/tc/birddex?id=${target.id}&guild_id=${msg.guildId}`)
-                                    .setEmoji("🐦")
-                            ),
-                        ],
-                    });
-                }
-                break;
+
             case "summoned":
             case "summons":
                 {
@@ -942,7 +909,7 @@ d20.on("messageCreate", async (msg) => {
                 for (let { names, callback } of _commands) {
                     // console.log(command, names);
                     if (names.includes(command)) {
-                        await msg.channel.sendTyping();
+                        // await msg.channel.sendTyping();
                         callback(msg, options);
                         break;
                     }
