12af6539f8c8c4aa437f3a9d5cf3d037220429ed
CyanMarine <58150571+PrincessCyanMarine@users.noreply.github.com>
2023-10-19 14:46:02 -0300

    gitpull button

--- src/interactions/button/krystal.ts
+++ src/interactions/button/krystal.ts
@@ -2,25 +2,29 @@ import { GuildMember } from "discord.js";
 import { database, testing } from "../..";
 import { krystal } from "../../clients";
 import { colors, isRestarting, triviumGuildId } from "../../common/variables";
-import { restart, stop, update } from "../../common/functions";
+import { gitpull, restart, stop, update } from "../../common/functions";
 
 krystal.on("interactionCreate", async (interaction) => {
     if (!interaction.isButton()) return;
     if (testing && interaction.channelId != "892800588469911663") return;
     else if (!testing && interaction.channelId == "892800588469911663") return;
     // console.log(interaction.customId);
-    if (["restart", "update", "stop"].includes(interaction.customId)) {
+    if (["restart", "update", "stop", "gitpull"].includes(interaction.customId)) {
         if (isRestarting()) {
             interaction.reply({ content: "Already restarting", ephemeral: true });
             return;
         }
-        interaction.reply({ content: { restart: "Restarting", update: "Updating", stop: "Stopping" }[interaction.customId], ephemeral: true });
+        interaction.reply({
+            content: { restart: "Restarting", update: "Updating", stop: "Stopping", gitpull: "Pulling" }[interaction.customId],
+            ephemeral: true,
+        });
         let act = {
+            gitpull,
             restart,
             update,
             stop,
         };
-        act[interaction.customId as "restart" | "update" | "stop"]();
+        act[interaction.customId as "restart" | "update" | "stop" | "gitpull"]();
     }
 
     if (interaction.customId.startsWith("colors")) {
