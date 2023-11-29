d632ca6b1f26885abc1aa9820ef1e5772d0087b2
CyanMarine <58150571+PrincessCyanMarine@users.noreply.github.com>
2023-11-29 17:26:26 -0300

    ertudjxfgjndtr6yfiokgygj6trydxfrjgrfjtg

--- src/common/index.ts
+++ src/common/index.ts
@@ -330,6 +330,13 @@ d20.on("messageCreate", async (msg) => {
                 }
                 break;
             }
+            case "items": {
+                let items = Object.entries(Inventory.ITEMS)
+                    .sort((a, b) => a[1].id - b[1].id)
+                    .map(([id, item]) => `${item.id}: ${item.name}`);
+                msg.reply(`\`\`\`\n${items.join("\n")}\n\`\`\``);
+                break;
+            }
             case "unequip":
             case "equip": {
                 let index = parseInt(options[1]);
